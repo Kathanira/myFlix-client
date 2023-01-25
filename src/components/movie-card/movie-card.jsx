88cd9fb7c9afc7467b1f60ec3861bfe66e55db8b
Kathanira <112494140+Kathanira@users.noreply.github.com>
2023-01-25 12:53:21 +0100

    updated

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -39,28 +39,4 @@ MovieCard.propTypes = {
 };
 
 
-/*import React from 'react';
-import PropTypes from 'prop-types';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 
-import './movie-card.scss';
-
-export class MovieCard extends React.Component {
-  render() {
-    const { movie, onMovieClick } = this.props;
-
-    return (
-      <div onClick={() => onMovieClick(movie)} className="movie-card">{movie.Title}</div>
-    );
-  }
-}
-
-MovieCard.propTypes = {
-  movie: PropTypes.shape({
-    Title: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string.isRequired
-  }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
-};*/
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -154,31 +154,4 @@ RegistrationView.propTypes = {register: PropTypes.shape({
 }),
 };
 
- /* return (
-    <form>
-      <h1>New User Registration</h1>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <label>
-        Email:
-        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-      </label>
-      <label>
-        Birthday:
-        <input type="birthday" value={birthday} onChange={e => setBirthday(e.target.value)} />
-      </label>
-      <button type="submit" onClick={handleSubmit}>Register</button>
-      <button type="button" onClick={() => {props.onBackClick(null);}}>Return to Login Page</button>
-    </form>
-  );
-}
-
-RegistrationView.propTypes = {
-  onRegistration: PropTypes.func.isRequired,
-};*/
+ 
